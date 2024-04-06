6f665481c2e83b9e193f1fe58e2a40ae26d7396e
Sylvati <LatiAuthor@gmail.com>
2024-04-06 03:49:13 -0400

    got rid of all that yap
    (not useful code rn)

--- src/main/java/lati/items/tools/BetterTool.java
+++ src/main/java/lati/items/tools/BetterTool.java
@@ -2,7 +2,6 @@ package lati.items.tools;
 
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
-import lati.items.tools.helper.ToolAttackUtil;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
@@ -15,7 +14,6 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tier;
@@ -98,13 +96,6 @@ public class BetterTool extends Item {
     }
 
     //Atacking Logic (hooh boy this is where it gets cringe)
-
-    /*TODO: incomplete lol
-    @Override
-    public boolean onLeftClickEntity(ItemStack stack, Player player, Entity entity) {
-        return ToolAttackUtil.attackEntity(stack, player, entity);
-    }*/
-
 
     //Harvesting Logic (:3)
 
